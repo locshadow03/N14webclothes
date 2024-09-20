73f75f757794812977d732e13829652792ad9db3
locshadow03 <locchuvan89@gmail.com>
2024-09-20 21:22:21 +0700

    create project

---
+++ webclothesservice/src/main/java/com/webclothes/webclothesservice/WebclothesserviceApplication.java
@@ -0,0 +1,13 @@
+package com.webclothes.webclothesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebclothesserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebclothesserviceApplication.class, args);
+    }
+
+}
---
+++ webclothesservice/src/test/java/com/webclothes/webclothesservice/WebclothesserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webclothes.webclothesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebclothesserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
